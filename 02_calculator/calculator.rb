2bfaf14928b4172bd64efd908fe7f240b09dcaaf
Nate Wooten <njwooten@outlook.com>
2017-10-15 19:45:11 -0400

    My calculator.rb solution.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+
+
+def add (val1, val2)
+  return val1 + val2
+end
+
+def subtract (val1, val2)
+  return val1 - val2
+end
+
+def sum(array)
+    array.inject(0, :+)
+end
+
+def multiply(*numbers)
+  result = 1
+  numbers.each { |n| result = result * n }
+  result
+end
+
+def power(val1, val2)
+  return val1 ** val2
+end
+
+class Integer
+  def factorial
+    self <= 1 ? 1 : self * (self - 1).factorial
+  end
+end
